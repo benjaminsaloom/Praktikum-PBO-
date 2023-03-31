3dae466b3671fa0b92453cca5ace15e22ab071bf
benjaminsaloom <100419126+benjaminsaloom@users.noreply.github.com>
2023-04-01 00:28:58 +0700

    Add files via upload

---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,39 @@
+// Nama: Benjamin Franklin Saloom Sipayung
+// NIM : 24060121130047
+
+class Titik {
+		double absis;
+		double ordinat;
+		static int counterTitik;
+		
+		Titik(){
+			absis =0.0;
+			ordinat= 0.0;
+			counterTitik ++;
+		}	
+		
+		Titik(double a, double o){
+			absis = a;
+			ordinat= o;
+			counterTitik ++;
+		}	
+		void setAbsis (double a) {
+			absis = a;
+			}
+			
+		void setOrdinat (double o) {
+			ordinat = o;
+			}
+		
+		double getAbsis() {
+			return absis;
+			}
+			
+		double getOrdinat() {
+			return ordinat;
+			}
+			
+		static double counterTitik(){
+			return counterTitik;
+			}	
+}
